5f976e27a344e6508b0b949f72892d0ca3dd5e01
Alejandro Avendaño <alejandro.ave@gmail.com>
2015-03-19 16:03:36 -0600

    prueba_personaje_terreno
    Pruebas de personaje con terreno y física

---
+++ AveGame/Assets/PersonajeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonajeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
